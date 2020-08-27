2744bb4d51945773750a34905366957f7d6fc307
alexcirilo <alexcirilo@github.com>
2020-08-27 16:09:57 -0300

    comentário thread

--- src/cursojava/thread/AulaThread.java
+++ src/cursojava/thread/AulaThread.java
@@ -2,13 +2,14 @@ package cursojava.thread;
 
 public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
-		
-		for(int pos =0; pos<10; pos++) {
+
+		for (int pos = 0; pos < 10; pos++) {
 			
-			Thread.sleep(1000);
-			
+			/*Quero executar esse envio com um tempo de parada, ou com tempo determinado*/
+			Thread.sleep(1000); // Tempo de execução (1s)
+
 			System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
 		}
-		
+
 	}
 }
